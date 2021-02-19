89cc923b66ee5448d34b5a9632ef1e6e6bfcadf9
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2021-02-19 19:33:26 +0000

    ahh

--- static/js/main.js
+++ static/js/main.js
@@ -89,3 +89,23 @@ function initMap() {
     zoom: 8,
   });
 }
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 5.738548696970146, lng: 0.03265251065119554 };
+
+
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 20,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    zoom: 4,
+  });
+}
+     
