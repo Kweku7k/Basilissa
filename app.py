4e7b19c9559ebc5252903827267ca1d9c0e9a98b
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2021-02-26 10:10:02 +0000

    u[

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user,logout_user,current_user,LoginManager
 import urllib.request, urllib.parse
 import urllib
-from models import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+from models import *
+
 login_manager = LoginManager(app)
 
 
